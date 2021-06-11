c20b6c85c89c29b4c0a0e1f967d5d4d6216e4190
songmiO <dhthd1060@gmail.com>
2021-06-11 15:03:15 +0900

    Add: first commit. 초기 세팅 완료.

---
+++ src/pages/hayoungGam/List/List.js
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+class List extends Component {
+  render() {
+    return <p>List hayoungpage</p>;
+  }
+}
+
+export default List;
---
+++ src/pages/songmiOh/Login/Login.js
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+class Login extends Component {
+  render() {
+    return <p>Login songmipage</p>;
+  }
+}
+
+export default Login;
