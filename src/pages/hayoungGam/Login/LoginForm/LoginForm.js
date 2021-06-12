d3e261674b225db82baad5aebd5ba4942d39fdb4
Hayoung Gam <hahhyoung@gmail.com>
2021-06-13 01:54:59 +0900

    Add: Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/hayoungGam/Login/LoginForm/LoginForm.js
+++ src/pages/hayoungGam/Login/LoginForm/LoginForm.js
@@ -3,8 +3,23 @@ import { withRouter } from 'react-router';
 import './LoginForm.scss';
 
 class LoginForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      idValue: '',
+      pwValue: '',
+    };
+  }
   goToList = () => {
     this.props.history.push('/list-hayoung');
+  };
+
+  handleIdInput = event => {
+    this.setState({ idValue: event.target.value });
+  };
+
+  handlePwInput = event => {
+    this.setState({ pwValue: event.target.value });
   };
 
   render() {
@@ -14,11 +29,13 @@ class LoginForm extends Component {
           className="login-form__input"
           type="text"
           placeholder="전화번호, 사용자 이름 또는 이메일"
+          onChange={this.handleIdInput}
         />
         <input
           className="login-form__input"
           type="password"
           placeholder="비밀번호"
+          onChange={this.handlePwInput}
         />
         <button className="login-form__btn" onClick={this.goToList}>
           로그인
