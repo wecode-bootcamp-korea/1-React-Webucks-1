8c753b8a87062e165d0571e9fd1e127001f81ee9
Hayoung Gam <hahhyoung@gmail.com>
2021-06-13 03:17:32 +0900

    Add: Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/hayoungGam/Login/LoginForm/LoginForm.js
+++ src/pages/hayoungGam/Login/LoginForm/LoginForm.js
@@ -8,36 +8,67 @@ class LoginForm extends Component {
     this.state = {
       idValue: '',
       pwValue: '',
+      idInputClassName: 'login-form__input',
+      pwInputClassName: 'login-form__input',
     };
   }
+
   goToList = () => {
     this.props.history.push('/list-hayoung');
   };
 
   handleIdInput = event => {
-    this.setState({ idValue: event.target.value });
+    this.setState({ idValue: event.target.value }, () => {
+      this.state.idValue.includes('@')
+        ? this.setState({
+            idInputClassName: 'login-form__input active',
+          })
+        : this.setState({
+            idInputClassName: 'login-form__input',
+          });
+    });
   };
 
   handlePwInput = event => {
-    this.setState({ pwValue: event.target.value });
+    const PASSWORD_MIN_LENGTH = 5;
+    this.setState({ pwValue: event.target.value }, () => {
+      this.state.pwValue.length >= PASSWORD_MIN_LENGTH
+        ? this.setState({
+            pwInputClassName: 'login-form__input active',
+          })
+        : this.setState({
+            pwInputClassName: 'login-form__input',
+          });
+    });
   };
 
   render() {
+    const { idInputClassName } = this.state;
+    const { pwInputClassName } = this.state;
     return (
       <form className="login-form">
         <input
-          className="login-form__input"
+          className={idInputClassName}
           type="text"
           placeholder="전화번호, 사용자 이름 또는 이메일"
           onChange={this.handleIdInput}
         />
         <input
-          className="login-form__input"
+          className={pwInputClassName}
           type="password"
           placeholder="비밀번호"
           onChange={this.handlePwInput}
         />
-        <button className="login-form__btn" onClick={this.goToList}>
+        <button
+          className="login-form__btn"
+          onClick={this.goToList}
+          disabled={
+            !(
+              idInputClassName === 'login-form__input active' &&
+              pwInputClassName === 'login-form__input active'
+            )
+          }
+        >
           로그인
         </button>
       </form>
