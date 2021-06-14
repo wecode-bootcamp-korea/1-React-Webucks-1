ec6fe5e6f1f925140ca739f51d2a18a057a86b5d
Hayoung Gam <hahhyoung@gmail.com>
2021-06-14 11:53:55 +0900

    Add: 리스트, 상세 페이지에 Header, Footer 추가

--- src/pages/hayoungGam/Detail/Detail.js
+++ src/pages/hayoungGam/Detail/Detail.js
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import Header from 'components/Header/Header';
+import Footer from 'components/Footer/Footer';
 import DetailTitle from './DetailTitle/DetailTitle';
 import DrinkDetailImageViewer from './DrinkDetailImageViewer/DrinkDetailImageViewer';
 import DrinkInfo from './DrinkInfo/DrinkInfo';
@@ -8,16 +10,20 @@ import './Detail.scss';
 class Detail extends Component {
   render() {
     return (
-      <div className="container">
-        <DetailTitle />
-        <section className="drink-detail-area">
-          <DrinkDetailImageViewer />
-          <div className="drink-infos">
-            <DrinkInfo />
-            <DrinkReview />
-          </div>
-        </section>
-      </div>
+      <>
+        <Header />
+        <div className="container">
+          <DetailTitle />
+          <section className="drink-detail-area">
+            <DrinkDetailImageViewer />
+            <div className="drink-infos">
+              <DrinkInfo />
+              <DrinkReview />
+            </div>
+          </section>
+        </div>
+        <Footer />
+      </>
     );
   }
 }
--- src/pages/hayoungGam/List/List.js
+++ src/pages/hayoungGam/List/List.js
@@ -1,13 +1,19 @@
 import { Component } from 'react';
+import Footer from 'components/Footer/Footer';
+import Header from 'components/Header/Header';
 import DrinkSection from './DrinkSection/DrinkSection';
 import './List.scss';
 
 class List extends Component {
   render() {
     return (
-      <div className="container">
-        <DrinkSection />
-      </div>
+      <>
+        <Header />
+        <div className="container">
+          <DrinkSection />
+        </div>
+        <Footer />
+      </>
     );
   }
 }
