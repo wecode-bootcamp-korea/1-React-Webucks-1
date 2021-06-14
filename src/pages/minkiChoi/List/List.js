7159aa19a5f6135f14287179809d0e57281e2d3a
samankey <samankeyc@gmail.com>
2021-06-14 13:50:49 +0900

    Add: 각 페이지 레이아웃 구성

--- src/pages/minkiChoi/List/List.js
+++ src/pages/minkiChoi/List/List.js
@@ -1,8 +1,115 @@
 import { Component } from 'react';
+import './List.scss';
 
 class List extends Component {
   render() {
-    return <p>List minkipage</p>;
+    return (
+      <div className="List">
+        <div className="container">
+          <nav className="navi">
+            <a className="naviTitle" href="">
+              Webucks
+            </a>
+            <div className="naviMenu">
+              <a href="">COFFEE</a>
+              <a href="">MENU</a>
+              <a href="">STORE</a>
+              <a href="">WHAT'S NEW</a>
+            </div>
+          </nav>
+          <header className="menuIntro">
+            <p className="menuName">콜드 브루 커피</p>
+            <img src="./Images/minkiChoi/coffee-cup.png" alt="coffee-cup" />
+            <p className="menuDescription">
+              디카페인 에스프레소 샷 추가 기능 (일부 음료 제외)
+            </p>
+          </header>
+          <section className="menuImage">
+            <div>
+              <a href="">
+                <img src="./Images/minkiChoi/ringo.jpg" alt="" />
+              </a>
+              <div className="shopProductName">ddd</div>
+            </div>
+            <div>
+              <a href="">
+                <img src="./Images/minkiChoi/vetty.jpg" alt="" />
+              </a>
+              <div className="shopProductName">ddd</div>
+            </div>
+            <div>
+              <a href="">
+                <img src="./Images/minkiChoi/deva.jpg" alt="" />
+              </a>
+              <div className="shopProductName">ddd</div>
+            </div>
+            <div>
+              <a href="">
+                <img src="./Images/minkiChoi/glory.jpg" alt="" />
+              </a>
+              <div className="shopProductName">ddd</div>
+            </div>
+            <div>
+              <a href="">
+                <img src="./Images/minkiChoi/liny.jpg" alt="" />
+              </a>
+              <div className="shopProductName">ddd</div>
+            </div>
+            <div>
+              <a href="">
+                <img src="./Images/minkiChoi/malo.jpg" alt="" />
+              </a>
+              <div className="shopProductName">ddd</div>
+            </div>
+            <div>
+              <a href="">
+                <img src="./Images/minkiChoi/plain.jpg" alt="" />
+              </a>
+              <div className="shopProductName">ddd</div>
+            </div>
+            <div>
+              <a href="">
+                <img src="./Images/minkiChoi/walter.jpg" alt="" />
+              </a>
+              <div className="shopProductName">ddd</div>
+            </div>
+            <div>
+              <a href="">
+                <img src="./Images/minkiChoi/breanna.jpg" alt="" />
+              </a>
+              <div className="shopProductName">ddd</div>
+            </div>
+            <div>
+              <a href="">
+                <img src="./Images/minkiChoi/cecil.jpg" alt="" />
+              </a>
+              <div className="shopProductName">ddd</div>
+            </div>
+          </section>
+          <header className="menuIntro">
+            <p className="menuName">브루드 커피</p>
+            <img src="./Images/minkiChoi/coffee-cup.png" alt="coffee-cup" />
+            <p className="menuDescription">
+              디카페인 에스프레소 샷 추가 기능 (일부 음료 제외)
+            </p>
+          </header>
+          <section className="menuImage">
+            <a href="">
+              <div className="wrapper">
+                <img src="./Images/minkiChoi/ryo.jpg" alt="" />
+              </div>
+              <div className="shopProductName">ddd</div>
+            </a>
+            <a href="">
+              <div className="wrapper">
+                <img src="./Images/minkiChoi/duncan.jpg" alt="" />
+              </div>
+              <div className="shopProductName">ddd</div>
+            </a>
+          </section>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/pages/minkiChoi/Login/Login.js
+++ src/pages/minkiChoi/Login/Login.js
@@ -1,8 +1,30 @@
 import { Component } from 'react';
+import './Login.scss';
 
 class Login extends Component {
   render() {
-    return <p>Login minkipage</p>;
+    return (
+      <div className="Login">
+        <div className="container">
+          <p>Webucks</p>
+          <input
+            type="text"
+            className="inputId"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+          />
+          <input type="password" className="inputPw" placeholder="비밀번호" />
+          <button
+            type="button"
+            className="loginBtn active"
+            onclick="location='./list.html'"
+            disabled
+          >
+            로그인
+          </button>
+          <a href="">비밀번호를 잊으셨나요?</a>
+        </div>
+      </div>
+    );
   }
 }
 
