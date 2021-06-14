efac7daee259c7ecc2c19a1a2a79eb098a5aeb25
Hayoung Gam <hahhyoung@gmail.com>
2021-06-14 13:42:38 +0900

    Modify: LikeBtn 컴포넌트용 SCSS 생성

--- src/components/hayoungGam/LikeBtn/LikeBtn.js
+++ src/components/hayoungGam/LikeBtn/LikeBtn.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons';
 import { faHeart as fasHeart } from '@fortawesome/free-solid-svg-icons';
+import './LikeBtn.scss';
 
 class LikeBtn extends Component {
   constructor() {
