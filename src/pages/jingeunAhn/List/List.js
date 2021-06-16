0f5582b926a7a0eb35c829e4aa074cc150a3e9da
안진근 <allmetoo11@gmail.com>
2021-06-16 17:37:09 +0900

    list Rrayout

--- src/pages/jingeunAhn/List/List.js
+++ src/pages/jingeunAhn/List/List.js
@@ -4,7 +4,7 @@ class List extends Component {
   render() {
     return (
       <>
-        <nav className="detail_nav_container">
+        <header className="detail_nav_container">
           <h6 className="nav_title">WeCoffeeTalgia</h6>
           <ul className="nav_list">
             <li>
@@ -20,7 +20,57 @@ class List extends Component {
               <a href=" ">what's new</a>
             </li>
           </ul>
-        </nav>
+        </header>
+
+        <section class="coffee_int_bar">
+          <h3 class="coffee_int_title"> Espresso </h3>
+          <i class="coffee_int_icon"></i>
+          <p class="coffee_int_detail"> 디카페인 에스프레소 샷 추가 가능 (일부 음료 제외) </p>
+        </section>
+
+        <div>
+          <figure>
+            <img src="./커피이미지/에스프레소.jpg">
+            <figcaption>에스프레소</figcaption>
+          </figure>
+
+          <figure>
+            <img src="./커피이미지/아메리카노.jpg">
+            <figcaption>아메리카노</figcaption>
+          </figure>
+
+          <figure>
+            <img src="./커피이미지/에스프레소 마키아또.jpg">
+            <figcaption>에스프레소 마키아또</figcaption>
+          </figure>
+
+          <figure>
+            <img src="./커피이미지/에스프레소 콘파냐.jpg">
+            <figcaption>에스프레소 콘파냐</figcaption>
+          </figure>
+
+          <figure>
+            <img src="./커피이미지/카페라떼.jpg">
+            <figcaption>카페라떼</figcaption>
+          </figure>
+
+          <figure>
+            <img src="./커피이미지/카페모카.jpg">
+            <figcaption>카페모카</figcaption>
+          </figure>
+
+          <figure>
+            <img src="./커피이미지/카푸치노.jpg">
+            <figcaption>카푸치노</figcaption>
+          </figure>
+
+          <figure>
+            <img src="./커피이미지/플랫 화이트.jpg">
+            <figcaption>플랫화이트</figcaption>
+          </figure>
+        </div>
+  <script src=""></script>  
+
       </>
     );
   }
