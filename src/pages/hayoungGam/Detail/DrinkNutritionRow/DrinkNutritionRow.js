43b5b9cc2fafe1989c810afd6df8ffd2665437de
Hayoung Gam <hahhyoung@gmail.com>
2021-06-16 15:41:05 +0900

    Refactor: 상세페이지 영양정보 테이블 불필요한 div 삭제

--- src/pages/hayoungGam/Detail/DrinkNutritionRow/DrinkNutritionRow.js
+++ src/pages/hayoungGam/Detail/DrinkNutritionRow/DrinkNutritionRow.js
@@ -5,14 +5,12 @@ class DrinkNutritionRow extends Component {
   render() {
     const { kor_name, unit, amount } = this.props;
     return (
-      <div className="drink-nutrition__table__row">
-        <dl className="drink-nutrition__table__desc-list">
-          <dt>
-            {kor_name} ({unit})
-          </dt>
-          <dd>{amount}</dd>
-        </dl>
-      </div>
+      <dl className="drink-nutrition__table__row">
+        <dt>
+          {kor_name} ({unit})
+        </dt>
+        <dd>{amount}</dd>
+      </dl>
     );
   }
 }
