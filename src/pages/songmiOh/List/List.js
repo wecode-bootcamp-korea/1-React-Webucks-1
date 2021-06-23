1a710cd6d30bd1cc4d31831a612b549f3312abc1
songmiO <dhthd1060@gmail.com>
2021-06-23 14:44:38 +0900

    Modify : List 페이지 수정

--- src/pages/songmiOh/List/List.js
+++ src/pages/songmiOh/List/List.js
@@ -1,34 +1,6 @@
 import { Component } from 'react';
 import './List.scss';
 
-class List extends Component {
-  // render() {
-  //   return (
-  //     <div className="list">
-  //       <div className="listBox">
-  //         <nav className="naviBar">
-  //           <a className="naviTitle" href="#">
-  //             Wecafevin
-  //           </a>
-  //           <div className="cafeMenu">
-  //             <a href="#">COFFEE</a>
-  //             <a href="#">MENU</a>
-  //             <a href="#">STORE</a>
-  //             <a href="#">REWARDS</a>
-  //             <a href="#">NEWITEM</a>
-  //           </div>
-  //         </nav>
-  //         <header className="menuSelection">
-  //           <p className="menuName">콜드 브루 커피</p>
-  //           <p className="menuIntroduction">
-  //             디카페인 에스프레소 샷 추가 기능(일부음료 제외)
-  //           </p>
-  //           <ListForm />
-  //         </header>
-  //       </div>
-  //     </div>
-  //   );
-  // }
-}
+class List extends Component {}
 
 export default List;
