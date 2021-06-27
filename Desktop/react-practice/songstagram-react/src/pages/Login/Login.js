88c0c2ce163ddf9387964963890ae1c9b744f3f7
songmiO <dhthd1060@gmail.com>
2021-06-27 22:48:11 +0900

    Add : 첫 파일 추가

---
+++ Desktop/react-practice/songstagram-react/src/pages/Login/Login.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./Login.scss";
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="Login">
+        <header className="container">
+          <p>songstagram</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Login;
